776e7306b5610ccb7dc540e4d2d30af59f0b0ab0
Gustavo H. S. Oliveira <gustax.dev@gmail.com>
2023-10-27 08:22:19 -0300

    Creating templates

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -6,13 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/movie')]
 class MovieController extends AbstractController
 {
-    #[Route('/movie', name: 'app_movie')]
+    #[Route('/', name: 'Movies')]
     public function index(): Response
     {
-        return $this->render('movie/index.twig', [
-            'controller_name' => 'MovieController',
-        ]);
+        $movies = [
+            ['title' => 'Avengers: End Game'],
+            ['title' => 'Sonic'],
+            ['title' => 'Mario Bros.'],
+        ];
+
+        return $this->render('movie/index.twig', ['movies' => $movies]);
+    }
+
+    #[Route('/{id}', name: 'Movie')]
+    public function movie(int $id): Response
+    {
+        $movie = [
+            'id' => $id,
+            'title' => 'Avengers',
+            'description' => 'An awesome movie!'
+        ];
+
+        return $this->render('movie/movie.twig', $movie);
     }
 }
